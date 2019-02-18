f0fcdfc89cca9f7604e30927876392f34ee845a5
AnnZh <ann.zhurok@gmail.com>
2019-02-19 00:50:04 +0300

    Done3

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -8,6 +8,13 @@ namespace PadawansTask5
         {
             if (source == null)
                 throw new ArgumentNullException();
+            
+                if (source.Length == 0)
+                    throw new ArgumentException();
+
+            
+   
+
             // put your code here
             //throw new NotImplementedException();
             for (int i = 0; i < source.Length/2; i++)
